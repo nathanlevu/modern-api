6de6605b504a2061d3cdbbfabeb0e9e34bf86c35
Nathan Le <nathan@Maomao.local>
2018-06-04 18:47:15 +0700

    set up server

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 const hapi = require('hapi');
 
 const server = hapi.server({
-    
+    port : 6969,
+    host : 'localhost'
 });
+
+
+const init = async () => {
+
+    server.route({
+        method : 'GET',
+        path: '/',
+        handler: function(request, reply){
+            return '<h1>Oh Fucking Shit</h1>';
+        }
+    });
+
+    await server.start();
+    console.log('Server running at: ' + server.info.uri);
+}
+
+init();
